bdb2b8f9bda5a9c6eb173b67250c2b24c58f6321
Ganesh-kaladi <sivaganeshkaladi97011@gmail.com>
2025-05-26 11:59:38 +0530

    Update allProductSlice.js

--- frontend/src/slice/allProductSlice.js
+++ frontend/src/slice/allProductSlice.js
@@ -1,6 +1,8 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
+const base = "hi-shop.up.railway.app";
+
 export const getAllProducts = createAsyncThunk(
   "allproducts/getAllProducts",
   async function (thunkApi) {
